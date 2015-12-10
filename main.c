2228aaaef15f8d733fc0ffd542f8c5f1862a07e5
Jonasz Czerepko <jonasz.czerepko@gmail.com>
2015-12-10 20:03:52 +0100

    Code formatting in main.c.

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
  * main.c
  *
  *  Created on: 9 gru 2015
- *      Author: Luke
+ *      Author: varsheim
  */
 
 #include <avr/io.h>
@@ -12,7 +12,7 @@
 #include "dioda.h"
 
 
-int main(void){
+int main(void) {
 
 	DDR(LED_PORT) |= LED_PIN;
 	PORT(LED_PORT) |= LED_PIN;
@@ -29,26 +29,27 @@ int main(void){
 	interrupt_init();
 	sei();
 
+	while(1) {
 
-	while(1){
-
-		if(isButtonPressed(BUTTON1_PIN))
+		if(isButtonPressed(BUTTON1_PIN)) {
 			blinking_freq = 2000;
+        }
 
-		if(isButtonPressed(BUTTON2_PIN))
+		if(isButtonPressed(BUTTON2_PIN)) {
 			blinking_freq = 50;
+        }
 
-		if(!iJustToggled && (counter % blinking_freq == 0)){
+		if(!iJustToggled && (counter % blinking_freq == 0)) {
 			toggleLED(LED_PIN);
 			iJustToggled = 1;
-
 		}
 	}
 }
 
-ISR(TIMER1_COMPA_vect){ //POWINNO sie wykonac co 1ms
-counter++;
-iJustToggled = 0;
-if(counter > 6000)
-	counter = 1;
+ISR(TIMER1_COMPA_vect) { 
+    counter++;
+    iJustToggled = 0;
+    if(counter > 6000) {
+        counter = 1;
+    }
 }
